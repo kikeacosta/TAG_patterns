77ad1e9a356501d0f03cac03cdff92153ef83dbf
giancarlocamarda <carlo-giovanni.camarda@ined.fr>
2021-06-11 23:41:45 +0200

    Improved version of the model presented today on the last slides (on simulated data)

--- R/Test2Populations.R
+++ R/Test2Populations.R
@@ -26,8 +26,8 @@ e1 <- c(29588,29295,29002,28707,28410,28111,27809,27504,27196,26883,26566,26245,
 e2 <- e1#c(38233,38253,38274,38295,38316,38337,38357,38375,38392,38406,38418,38427,38431,38432,38429,38420,38406,38386,38359,38326,38286,38238,38182,38118,38045,37962,37935,38020,38205,38476,38823,39234,39696,40198,40726,41267,41806,42329,42820,43261,43637,43930,44123,44199,44142,43937,43571,43033,42314,41410,40318,39212,38243,37382,36603,35881,35197,34528,33856,33165,32437,31660,30820,29907,28914,27836,26670,25417,24080,22669,21192,19663,18098,16516,14935,13375,11892,10526,9273,8129,7090,6152,5309,4556,3887,3296,2779,2327,1937,1601,1314,1072,867,697,556,440,345,269,208,159,121)
 
 ## increasing sample size?
-e1 <- e1
-e2 <- e2
+e1 <- e1*10
+e2 <- e2*10
 ## dimension & age
 m <- length(e1)
 x <- 1:m-1
@@ -44,18 +44,18 @@ lines(x, e2, col=2, lwd=2)
 ## component 1: simple exponential
 
 ## population 1 (pre-C19): lower mortality and more gompertzian
-alpha1 <- -6
+alpha1 <- -6.8
 alpha2 <- -0.7
 alphas <- c(alpha1, alpha2)
 X1 <- cbind(1, x)
 eta1 <- X1%*%alphas
 ## component 2: exponential + cos
-ome <- pi/85
-beta1 <- -11
-beta2 <- 0.11
-beta3 <- 0.7
+ome <- pi/45
+beta1 <- -13
+beta2 <- 0.12
+beta3 <- 0.8
 betas <- c(beta1, beta2, beta3)
-X2 <- cbind(1, x, sin(x*ome))
+X2 <- cbind(1, x, cos(x*ome))
 eta2 <- X2%*%betas
 ## overall (log-)mortality
 muT1 <- exp(eta1) + exp(eta2)
@@ -63,42 +63,60 @@ etaT1 <- log(muT1)
 
 ## plotting true log-mortality
 ## with components that we will disregard afterward
-rany <- c(-10, 1)
+rany <- c(-12, 0)
 plot(x, eta2, t="l", lwd=2, col=2, ylim=rany, axes=FALSE)
-yy <- 10^seq(-10, 2)
+yy <- 10^seq(-10, 0)
 axis(2, at=log(yy), labels=yy)
 axis(1);box()
 lines(x, eta1, t="l", lwd=2, col=1)
 lines(x, etaT1, col=4, lwd=2)
 
-## population 2 (C19-2020): higher mortality and more "sinousoid"
-## (higher mortality at higher ages)
-alpha1 <- -5.8
-alpha2 <- -0.7
-alphas <- c(alpha1, alpha2)
-X1 <- cbind(1, x)
-eta1 <- X1%*%alphas
-## component 2: exponential + cos
-ome <- pi/45
-beta1 <- -9.5
-beta2 <- 0.105
-beta3 <- 0.6
+
+## population 2: etaT2 = etaT1 + c + delta
+## true scaling factor
+cT <- 0.5
+## constructing true delta vector
+ome <- pi/40
+beta1 <- 0
+beta2 <- 0.02
+beta3 <- 1
 betas <- c(beta1, beta2, beta3)
 X2 <- cbind(1, x, cos(x*ome))
-eta2 <- X2%*%betas
-## overall (log-)mortality
-muT2 <- exp(eta1) + exp(eta2)
-etaT2 <- log(muT2)
+deltaT <- X2%*%betas
+deltaT <-  deltaT - mean(deltaT)
+plot(x, deltaT)
+## true log-mortality for the second population (C19-2020)
+etaT2 <- etaT1 + cT + deltaT
+muT2 <- exp(etaT2)
 
-## plotting true log-mortality
-## with components that we will disregard afterward
-rany <- c(-10, 1)
-plot(x, eta2, t="l", lwd=2, col=2, ylim=rany, axes=FALSE)
-yy <- 10^seq(-20, 2)
-axis(2, at=log(yy), labels=yy)
-axis(1);box()
-lines(x, eta1, t="l", lwd=2, col=1)
-lines(x, etaT2, col=4, lwd=2)
+# ## population 2 (C19-2020): higher mortality and more "sinousoid"
+# ## (higher mortality at higher ages)
+# alpha1 <- -5.8
+# alpha2 <- -0.7
+# alphas <- c(alpha1, alpha2)
+# X1 <- cbind(1, x)
+# eta1 <- X1%*%alphas
+# ## component 2: exponential + cos
+# ome <- pi/45
+# beta1 <- -9.5
+# beta2 <- 0.105
+# beta3 <- 0.6
+# betas <- c(beta1, beta2, beta3)
+# X2 <- cbind(1, x, cos(x*ome))
+# eta2 <- X2%*%betas
+# ## overall (log-)mortality
+# muT2 <- exp(eta1) + exp(eta2)
+# etaT2 <- log(muT2)
+
+# ## plotting true log-mortality
+# ## with components that we will disregard afterward
+# rany <- c(-10, 1)
+# plot(x, eta2, t="l", lwd=2, col=2, ylim=rany, axes=FALSE)
+# yy <- 10^seq(-20, 2)
+# axis(2, at=log(yy), labels=yy)
+# axis(1);box()
+# lines(x, eta1, t="l", lwd=2, col=1)
+# lines(x, etaT2, col=4, lwd=2)
 
 
 ## simulating deaths for both populations
@@ -112,7 +130,7 @@ lmx2 <- log(d2/e2)
 
 ## plotting simulated and true log-mortality
 rany <- range(etaT1, etaT2)
-plot(x, etaT1, t="l", lwd=2, col=2, ylim=rany, axes=TRUE)
+plot(x, etaT1, t="l", lwd=2, col=2, ylim=rany, axes=FALSE)
 yy <- 10^seq(-20, 2)
 axis(2, at=log(yy), labels=yy)
 axis(1);box()
@@ -225,14 +243,14 @@ tDDeta1 <- t(Deta1)%*%Deta1
 Ddelta <- diff(diag(m), diff=2)
 tDDdelta <- t(Ddelta)%*%Ddelta
 
-lambda.eta1 <- 10^2
-lambda.delta <- 10^4
+lambda.eta1 <- 10^3
+lambda.delta <- 10^3.5
 
 P <- adiag(lambda.eta1*tDDeta1,
            0,
            lambda.delta*tDDdelta)
 ## ridge penalty
-Pr <- 1e-6*diag(ncol(P))
+Pr <- 1e-4*diag(ncol(P))
 
 ## model matrix
 U0 <- rbind(diag(m), diag(m))
@@ -264,6 +282,61 @@ delta.hat <- betas[1:m+1+m]
 eta2.hat <- eta1.hat + c.hat + delta.hat
 
 
+# par(mfrow=c(1,3))
+# ## eta1
+# rany <-range(etaT1, etaT2, lmx1g, lmx2g, finite=TRUE)
+# ranx <- range(x)
+# plot(1, 1, t="n", xlim=ranx, ylim=rany, axes=FALSE, 
+#      xlab="age", ylab="mortality, log-scale",
+#      main="mortality age-pattern")
+# yy <- 10^seq(-7, 2)
+# axis(2, at=log(yy), labels=yy)
+# axis(1);box()
+# lines(x, eta1.hat, col=2, lwd=4)
+# lines(x, etaT1, col=4, lwd=2, lty=2)
+# for(i in 1:n1){
+#   segments(x0=low1[i], x1=up1[i],
+#            y0=lmx1g[i], y1=lmx1g[i], col=4, lwd=3)
+# }
+# ## eta2
+# plot(1, 1, t="n", xlim=ranx, ylim=rany, axes=FALSE, 
+#      xlab="age", ylab="mortality, log-scale",
+#      main="mortality age-pattern")
+# yy <- 10^seq(-7, 2)
+# axis(2, at=log(yy), labels=yy)
+# axis(1);box()
+# lines(x, eta2.hat, col=2, lwd=4)
+# lines(x, etaT2, col=4, lwd=2, lty=2)
+# for(i in 1:n1){
+#   segments(x0=low2[i], x1=up2[i],
+#            y0=lmx2g[i], y1=lmx2g[i], col=4, lwd=3)
+# }
+# 
+# plot(x, delta.hat, col=4, lwd=4, t="l",
+#      main=paste("c = ", signif(c.hat,4)))
+# abline(h=0, col=8, lty=3, lwd=2)
+# lines(x, deltaT, col=4, lty=2, lwd=2)
+# par(mfrow=c(1,1))
+
+H0 <- solve(GpP)
+Vbetas <- H0 %*% G %*% H0
+se.betas <- sqrt(diag(Vbetas))
+se.eta1 <- se.betas[1:m]
+se.c <- se.betas[m+1]
+se.delta <- se.betas[1:m+1+m]
+V.eta12 <- U %*% Vbetas %*% t(U)
+se.eta2 <- sqrt(diag(V.eta12))[1:m+m]
+
+
+eta1.hatL <- eta1.hat - 2*se.eta1
+eta1.hatU <- eta1.hat + 2*se.eta1
+eta2.hatL <- eta2.hat - 2*se.eta2
+eta2.hatU <- eta2.hat + 2*se.eta2
+c.hatL <- c.hat - 2*se.c
+c.hatU <- c.hat + 2*se.c
+delta.hatL <- delta.hat - 2*se.delta
+delta.hatU <- delta.hat + 2*se.delta
+
 par(mfrow=c(1,3))
 ## eta1
 rany <-range(etaT1, etaT2, lmx1g, lmx2g, finite=TRUE)
@@ -274,207 +347,55 @@ plot(1, 1, t="n", xlim=ranx, ylim=rany, axes=FALSE,
 yy <- 10^seq(-7, 2)
 axis(2, at=log(yy), labels=yy)
 axis(1);box()
-lines(x, eta1.hat, col=2, lwd=4)
-lines(x, etaT1, col=4, lwd=2, lty=2)
-for(i in 1:n1){
-  segments(x0=low1[i], x1=up1[i],
-           y0=lmx1g[i], y1=lmx1g[i], col=4, lwd=3)
-}
-## eta2
-plot(1, 1, t="n", xlim=ranx, ylim=rany, axes=FALSE, 
-     xlab="age", ylab="mortality, log-scale",
-     main="mortality age-pattern")
-yy <- 10^seq(-7, 2)
-axis(2, at=log(yy), labels=yy)
-axis(1);box()
-lines(x, eta2.hat, col=2, lwd=4)
-lines(x, etaT2, col=4, lwd=2, lty=2)
-for(i in 1:n1){
-  segments(x0=low2[i], x1=up2[i],
-           y0=lmx2g[i], y1=lmx2g[i], col=4, lwd=3)
-}
-
-plot(x, delta.hat, col=4, lwd=4, t="l",
-     main=paste("c = ", signif(c.hat,4)))
-abline(h=0, col=8, lty=3, lwd=2)
-par(mfrow=c(1,1))
-
-H0 <- solve(GpP)
-Vbetas <- H0 %*% G %*% H0
-
-se.betas <- sqrt(diag(Vbetas))
-se.eta1 <- se.betas[1:m]
-se.c <- se.betas[m+1]
-
-M <- 
-
-eta1.hat <- eta
-eta1.hatL <- eta1.hat - 2*se.eta1
-eta1.hatU <- eta1.hat + 2*se.eta1
-
-
-
-## population 2: 
-## ln(e_2 * \mu_2) = ln(e_2) + \eta_2(x) = ln(e_2) + \eta_1(x) + c + \delta(x)
-## with ln(e_2) + \eta_1(x) as offset
-y <- d2g
-e <- e2
-C <- C2
-y.st0 <- rep(y/len2, len2)
-fit0 <- Mort1Dsmooth(x=x, y=y.st0,
-                     offset=log(e),
-                     method=3, lambda=10^3)
-plot(fit0)
-eta.st <- fit0$logmortality
-eta <- eta.st
-max.it <- 100
-U <- cbind(1, diag(m))
-## penalty stuff
-D0 <- diff(diag(m), diff=2)
-D <- adiag(0, D0)
-tDD <- t(D)%*%D
-lambda <- 10^2.5
-P <- lambda*tDD
-Pr <- 1e-6*diag(ncol(P))
-for(it in 1:max.it){
-  gamma   <- exp(eta)
-  mu      <- c(C %*% gamma)
-  X       <- ( C * ((1 / mu) %*% t(gamma)) ) %*% U
-  w       <- as.vector(mu)
-  r       <- y - mu + C %*% (gamma * eta)
-  G       <- t(X) %*% (w * X) 
-  GpP     <- G + P +Pr
-  tXr     <- t(X) %*% r
-  
-  betas   <- solve(GpP, tXr)
-  eta.old <- eta
-  eta <- U%*%betas
-  dif.eta <- max(abs((eta - eta.old)/eta.old) )
-  if(dif.eta < 1e-04 & it > 4) break
-  cat(it, dif.eta, "\n")
-}
-H0 <- solve(GpP)
-Veta2 <- H0 %*% G %*% H0
-se.eta2 <- sqrt(diag(Veta2))
-eta2.hat <- eta
-eta2.hatL <- eta2.hat - 2*se.eta2
-eta2.hatU <- eta2.hat + 2*se.eta2
-## rate-of-aging
-Droa <- Dfun(m)
-Vroa2 <- Droa %*% Veta2 %*% t(Droa)
-se.roa2 <- sqrt(diag(Vroa2))
-roa2.hat <- Droa%*%eta2.hat
-roa2.hatL <- roa2.hat - 2*se.roa2
-roa2.hatU <- roa2.hat + 2*se.roa2
-
-## true roa
-roaT1 <- Droa%*%etaT1
-roaT2 <- Droa%*%etaT2
-
-
-
-
-par(mfrow=c(2,2))
-## eta1
-rany <-range(etaT1, etaT2, lmx1g, lmx2g, finite=TRUE)
-ranx <- range(x)
-plot(1, 1, t="n", xlim=ranx, ylim=rany, axes=FALSE, 
-     xlab="age", ylab="mortality, log-scale",
-     main="mortality age-pattern")
-yy <- 10^seq(-7, 2)
-axis(2, at=log(yy), labels=yy)
-axis(1);box()
-lines(x, eta1.hat, col=2, lwd=4)
-lines(x, etaT1, col=4, lwd=2, lty=2)
-for(i in 1:n1){
-  segments(x0=low1[i], x1=up1[i],
-           y0=lmx1g[i], y1=lmx1g[i], col=4, lwd=3)
-}
-## eta2
-plot(1, 1, t="n", xlim=ranx, ylim=rany, axes=FALSE, 
-     xlab="age", ylab="mortality, log-scale",
-     main="mortality age-pattern")
-yy <- 10^seq(-7, 2)
-axis(2, at=log(yy), labels=yy)
-axis(1);box()
-lines(x, eta2.hat, col=2, lwd=4)
-lines(x, etaT2, col=4, lwd=2, lty=2)
-for(i in 1:n1){
-  segments(x0=low2[i], x1=up2[i],
-           y0=lmx2g[i], y1=lmx2g[i], col=4, lwd=3)
-}
-## roa1
-rany <-range(roa1.hat, roa2.hat, finite=TRUE)
-ranx <- range(x)
-plot(1, 1, t="n", xlim=ranx, ylim=rany, axes=FALSE, 
-     xlab="age", ylab="rate-of-aging",
-     main="mortality age-pattern")
-axis(2)
-axis(1);box()
-abline(h=0, col=8, lwd=2, lty=3)
-lines(x, roa1.hat, col=2, lwd=4)
-lines(x, roaT1, col=4, lwd=2, lty=2)
-## roa2
-plot(1, 1, t="n", xlim=ranx, ylim=rany, axes=FALSE, 
-     xlab="age", ylab="rate-of-aging",
-     main="mortality age-pattern")
-axis(2)
-axis(1);box()
-abline(h=0, col=8, lwd=2, lty=3)
-lines(x, roa2.hat, col=2, lwd=4)
-lines(x, roaT2, col=4, lwd=2, lty=2)
-par(mfrow=c(1,1))
-
-## both roa
-rany <-range(roa1.hatL, roa1.hatU, roa2.hatL, roa2.hatU, finite=TRUE)
-plot(1, 1, t="n", xlim=ranx, ylim=rany, axes=FALSE, 
-     xlab="age", ylab="rate-of-aging",
-     main="mortality age-pattern")
-axis(2)
-axis(1);box()
-abline(h=0, col=8, lwd=2, lty=3)
 xx <- c(x, rev(x))
-yy <- c(roa1.hatL, rev(roa1.hatU))
+yy <- c(eta1.hatL, rev(eta1.hatU))
 polygon(xx, yy, col=adjustcolor(2, 0.5), border=adjustcolor(2, 0.5))
-lines(x, roa1.hat, col=2, lwd=4)
-
-yy <- c(roa2.hatL, rev(roa2.hatU))
-polygon(xx, yy, col=adjustcolor(4, 0.5), border=adjustcolor(2, 0.5))
-lines(x, roa2.hat, col=4, lwd=4)
-
-
-## working on the differences in roa
-eta12.hat <- c(eta1.hat, eta2.hat)
-U <- adiag(Droa, Droa)
-H <- cbind(diag(m), -diag(m))
-HU <- H%*%U
-delta <- HU%*%eta12.hat
-# delta1 <- roa1.hat-roa2.hat
-# range(delta-delta1)
-
-Veta12 <- adiag(Veta1, Veta2)
-dim(Veta12)
-Vdelta <- HU %*% Veta12 %*% t(HU)
-se.delta <- sqrt(diag(Vdelta))
-deltaL <- delta - 2*se.delta
-deltaU <- delta + 2*se.delta
+lines(x, eta1.hat, col=2, lwd=4)
+lines(x, etaT1, col=4, lwd=2, lty=2)
+for(i in 1:n1){
+  segments(x0=low1[i], x1=up1[i],
+           y0=lmx1g[i], y1=lmx1g[i], col=3, lwd=3)
+}
+## eta2
+plot(1, 1, t="n", xlim=ranx, ylim=rany, axes=FALSE, 
+     xlab="age", ylab="mortality, log-scale",
+     main="mortality age-pattern")
+yy <- 10^seq(-7, 2)
+axis(2, at=log(yy), labels=yy)
+axis(1);box()
+xx <- c(x, rev(x))
+yy <- c(eta2.hatL, rev(eta2.hatU))
+polygon(xx, yy, col=adjustcolor(2, 0.5), border=adjustcolor(4, 0.5))
+lines(x, eta2.hat, col=2, lwd=4)
+lines(x, etaT2, col=4, lwd=2, lty=2)
+for(i in 1:n1){
+  segments(x0=low2[i], x1=up2[i],
+           y0=lmx2g[i], y1=lmx2g[i], col=3, lwd=3)
+}
 
 ranx <- range(x)
-rany <- range(deltaL, deltaU)
-plot(1,1, xlim=ranx, ylim=rany, t="n",
-     xlab="ages", ylab="differences in rate-of-aging")
-abline(h=0, col=8, lwd=2, lty=3)
+rany <- range(delta.hatL,delta.hatU)
+plot(1, 1, t="n", xlim=ranx, ylim=rany,
+     main=paste("c = ", signif(c.hat,4), "+ -", signif(2*se.c,4)), axes=FALSE,
+     xlab="ages", ylab="delta")
+axis(2)
+axis(1);box()
+abline(h=0, col=8, lty=3, lwd=2)
 xx <- c(x, rev(x))
-yy <- c(deltaL, rev(deltaU))
-polygon(xx, yy, col=adjustcolor(4, 0.5), border=adjustcolor(2, 0.5))
-lines(x, delta, col=4, lwd=4)
+yy <- c(delta.hatL, rev(delta.hatU))
+polygon(xx, yy, col=adjustcolor(2, 0.5), border=adjustcolor(4, 0.5))
+lines(x, delta.hat, col=2, lwd=4)
+lines(x, deltaT, col=4, lwd=2, lty=2)
+legend("topleft", inset=0.1,
+       legend=c("True", "Simulated+Grouped", "Fit + 95% CI"),
+       col=c(4, 3, adjustcolor(2, 0.5)), pch=c(NA, NA, 15), 
+       pt.cex=2, lty=c(2,1,1),
+       lwd=c(2,3,4), cex=1.5)
+
+par(mfrow=c(1,1))
 
 
 
-library(mgcv)
-m0 <- gam(d2 ~ s(x), offset=log(e1)+eta1.hat, family=poisson())
-summary(m0)
-plot(m0)
 
 
 
